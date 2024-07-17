e26c2ed79a58d5c0d5affbd078f90dde9957fa6f
D1204416 <161418864+D1204416@users.noreply.github.com>
2024-07-17 10:54:33 +0800

    Bmi

--- Bmi/app/src/main/java/com/example/bmi/MainActivity.java
+++ Bmi/app/src/main/java/com/example/bmi/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.bmi;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResult;
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityResultLauncher<Intent> intentActivityResultLauncher;
     private double bmi;
+    private TextView value;
+    private TextView description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +43,70 @@ public class MainActivity extends AppCompatActivity {
                         // 寫另一個Activity回傳後,得到回傳的資料之後的做法
                         if(result.getData() != null && result.getResultCode() == Activity.RESULT_OK){
                             bmi = result.getData().getDoubleExtra("BMI",-1);
+                            updateUI();
                         }
                     }
                 }
         );
     }
 
-    public void GotoBMICalculate(View view){
+    public void GotoCalBMI(View view){
         Intent intent = new Intent(this,CalBMIActivity.class);
+        intentActivityResultLauncher.launch(intent);
+
+        if(value!= null){
+            value.setTextColor(Color.parseColor("#000000"));
+            description.setTextColor(Color.parseColor("#000000"));
+        }
+    }
+
+    public void updateUI(){
+        TextView tv_result = (TextView) findViewById(R.id.Result);
+        tv_result.setText(String.valueOf(bmi));
+
+        if(bmi<18.5){
+            value = (TextView) findViewById(R.id.thin_value);
+            description = (TextView) findViewById(R.id.thin_description);
+
+            value.setTextColor(Color.parseColor("#CC0000"));
+            description.setTextColor(Color.parseColor("#CC0000"));
+
+        } else if (bmi < 24) {
+            value = (TextView) findViewById(R.id.normal_value);
+            description = (TextView) findViewById(R.id.normal_description);
+
+            value.setTextColor(Color.parseColor("#68BE8D"));
+            description.setTextColor(Color.parseColor("#68BE8D"));
+
+        }else if (bmi < 27) {
+            value = (TextView) findViewById(R.id.heavy_value);
+            description = (TextView) findViewById(R.id.heavy_description);
+
+            value.setTextColor(Color.parseColor("#CC0000"));
+            description.setTextColor(Color.parseColor("#CC0000"));
+
+        }else if (bmi < 30) {
+            value = (TextView) findViewById(R.id.littlefat_value);
+            description = (TextView) findViewById(R.id.littlefat_description);
+
+            value.setTextColor(Color.parseColor("#CC0000"));
+            description.setTextColor(Color.parseColor("#CC0000"));
+
+        }else if (bmi < 35) {
+            value = (TextView) findViewById(R.id.middlefat_value);
+            description = (TextView) findViewById(R.id.middlefat_description);
+
+            value.setTextColor(Color.parseColor("#CC0000"));
+            description.setTextColor(Color.parseColor("#CC0000"));
+
+        }else{
+            value = (TextView) findViewById(R.id.toofat_value);
+            description = (TextView) findViewById(R.id.toofat_description);
+
+            value.setTextColor(Color.parseColor("#CC0000"));
+            description.setTextColor(Color.parseColor("#CC0000"));
+        }
+
+
     }
 }
