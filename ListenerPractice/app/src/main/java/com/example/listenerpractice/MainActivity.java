2733c34e86a04b754f02560420fb67c3ff17e7b2
D1204416 <161418864+D1204416@users.noreply.github.com>
2024-07-31 11:22:58 +0800

    0731課堂練習2

--- ListenerPractice/app/src/main/java/com/example/listenerpractice/MainActivity.java
+++ ListenerPractice/app/src/main/java/com/example/listenerpractice/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.listenerpractice;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button b = (Button) findViewById(R.id.big);
+        b.setOnClickListener(this);
+        Button s = (Button) findViewById(R.id.small);
+        s.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        TextView textsize = (TextView) findViewById(R.id.textsize);
+        TextView textview = (TextView) findViewById(R.id.textview);
+
+        float currentSizeSp = textsize.getTextSize()/getResources().getDisplayMetrics().scaledDensity;
+
+        if(view.getId() == R.id.big) {
+            textview.setTextSize(currentSizeSp + 5); // 變更尺寸
+            textsize.setText(String.valueOf(currentSizeSp + 5)); // 顯示尺寸
+        }else{
+            textview.setTextSize(currentSizeSp - 5); // 變更尺寸
+            textsize.setText(String.valueOf(currentSizeSp - 5)); // 顯示尺寸
+        }
+
     }
 }
